814b9d3e354ac3e8b9a0978295e18a0bc4dc7b16
Jose Zangarini <jzangarinim@gmail.com>
2023-04-19 14:02:56 -0400

    added functionality

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
+let domainsArray = [];
+
+function nameGenerator() {
+  const pronoun = ["the", "our"];
+  const adjective = ["great", "big", "strong", "colorful"];
+  const noun = ["jogger", "house", "raccoon", "table"];
+  const extension = [".com", ".us", ".org", ".io"];
+  let finalDomain = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    finalDomain = pronoun[i];
+    for (let j = 0; j < adjective.length; j++) {
+      finalDomain = pronoun[i] + adjective[j];
+      for (let k = 0; k < noun.length; k++) {
+        finalDomain = pronoun[i] + adjective[j] + noun[k];
+        for (let l = 0; l < extension.length; l++) {
+          finalDomain = pronoun[i] + adjective[j] + noun[k] + extension[l];
+          domainsArray.push(finalDomain);
+        }
+      }
+    }
+  }
+  return domainsArray;
+}
+
 window.onload = () => {
-  let body = document.body;
+  nameGenerator();
+
   let app = document.querySelector("#app");
+  app.style.border = "1px solid red";
+
+  let h1 = document.createElement("h1");
+  h1.textContent = "Domain Name Generator";
+  app.appendChild(h1);
+  h1.style.textAlign = "center";
+  h1.style.fontSize = "24px";
 
   let ul = document.createElement("ul");
   app.appendChild(ul);
 
-  let li = document.createElement("li");
-  ul.appendChild(li);
-  li.textContent = "ola";
+  for (let i = 0; i < domainsArray.length; i++) {
+    let li = document.createElement("li");
+    ul.appendChild(li);
+    li.textContent = domainsArray[i];
+  }
 };
